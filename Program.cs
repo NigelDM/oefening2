5bc13ab1ad5ad80e170079a478599c1151421136
Nigel <nigel.demaeght@gmail.com>
2021-02-12 13:07:00 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oefening2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
